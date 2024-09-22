cf742fd1dc664b62c5e7b27bc46d90c3f0b65c38
TU NGUYEN <trongtunguyen08@icloud.com>
2024-09-22 14:42:44 +0700

    Initial Commit

---
+++ NavigationStackProject/NavigationStackProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackProjectApp.swift
+//  NavigationStackProject
+//
+//  Created by Tu Nguyen on 22/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
